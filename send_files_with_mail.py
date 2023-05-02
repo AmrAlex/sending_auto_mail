e2843c8265b8b687bac14a7dc9ab61d31e41bd85
AmrAlex <90861129+AmrAlex@users.noreply.github.com>
2023-05-02 16:09:14 +0300

    Update send_files_with_mail.py

--- send_files_with_mail.py
+++ send_files_with_mail.py
@@ -1,67 +1,60 @@
-from exchangelib import DELEGATE,Credentials,Configuration, Account, Message, FileAttachment, HTMLBody,NTLM
-import os
-from exchangelib.protocol import BaseProtocol, NoVerifyHTTPAdapter
-username = 'user name'
-password = 'password'
+from exchangelib import DELEGATE,Credentials,Configuration, Account, CalendarItem, Message, Mailbox, FileAttachment, HTMLBody,NTLM, GSSAPI, SSPI
+import re
+import smtplib 
+from email.mime.multipart import MIMEMultipart 
+from email.mime.text import MIMEText 
+from email.mime.base import MIMEBase 
+from email import encoders
+import os,sys
+import getpass
+username = 'user_name'
+password = 'passwor'
 url = 'company url'
-email = 'mail '
-body='''
-                Dear All, </br></br>
-                                                            kindly find attached dashboard_files.</br>
-                                                            </br>
-                                                            BR,</br>
-                                                            Amr Ahmed.</br>
-                '''
+email = 'mail'
 
-mainPath=os.path.dirname(os.path.abspath(__file__))
-mainPath=mainPath+'/input/vas'
-attached_file=[]
-for filename in os.listdir(mainPath):
-    i=filename
-    attached=os.path.join(mainPath,i)
-    attached_file.append(attached)
-def send(filename=None,body=body):
-    filename = [] if filename is None else filename
-    # print(filename)
-    # Recipients
-    TO =["amr.ahmed.mostafa17@gmail.com"]
-    CC = ["amr.ahmed.mostafa17@gmail.com"]
-    BCC = []
-    # Email
-    subject = "Dash_Board_Files"
+class EmailSender:
     
-    attach = attached_file # attach file
+    @staticmethod 
+    def send(filename=None,body=""):
+        filename = [] if filename is None else filename
+       # print(filename)
+       # Recipients
+        TO = ['amr.ahmed.mostafa17@gmail.com']
+        CC = ['amr.ahmed.mostafa17@gmail.com']
+        BCC = []
+        # Email
+        subject = "CTC CallSR"
+        
+        attach = filename # attach file
 
-    try:
-        BaseProtocol.HTTP_ADAPTER_CLS = NoVerifyHTTPAdapter
-        credentials = Credentials(username, password )
-        config = Configuration(service_endpoint=url, credentials=credentials, auth_type=NTLM)
-        account = Account(email, credentials=credentials, autodiscover=False, config=config)
+        try:
+            credentials = Credentials(username, password )
+            config = Configuration(service_endpoint=url, credentials=credentials, auth_type=NTLM)
+            account = Account(email, credentials=credentials, autodiscover=False, config=config)
 
-        m = Message(
-            account=account,
-            subject=subject,
-            body=HTMLBody(body),
-            to_recipients=TO,
-            cc_recipients=CC,  
-            bcc_recipients=BCC,  
-        )
+            m = Message(
+                account=account,
+                subject=subject,
+                body=HTMLBody(body),
+                to_recipients=TO,
+                cc_recipients=CC,  
+                bcc_recipients=BCC,  
+            )
 
 
-        if len(attach)>0:
-            for f in attach:
+            if len(attach)>0:
                 for f in attach:
-                    with open(f,'rb') as fil:
-                        file = FileAttachment(name=os.path.basename(f), content=fil.read())
-                        m.attach(file)
+                    for f in attach:
+                        with open(f,'rb') as fil:
+                            file = FileAttachment(name=os.path.basename(f), content=fil.read())
+                            m.attach(file)
 
-        m.send()
-    except Exception as ex:
-        print("Failed to send mail")
-        print(ex)
-    else:
-        print("Successfully sent email")
+            m.send()
+        except Exception as ex:
+            print("Failed to send mail")
+            print(ex)
+        else:
+            print("Successfully sent email")
 
 
 
-send()
